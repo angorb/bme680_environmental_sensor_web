ccd5614074b7da4c9f616706e62c0a594416363f
nick brogna <nick@wemakepretty.com>
2023-06-10 22:36:31 -0600

    rough in web UI

--- app/resources/templates/view.php
+++ app/resources/templates/view.php
@@ -16,44 +16,83 @@ $client = new SensorClient("192.168.1.172");
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/main.css">
 
-    <title>Consciousness</title>
+    <title>Bosch Sensortec BME680</title>
 </head>
 
 <body>
-    <h1>Bosch Sensortec BME680</h1>
+    <div class="container">
+        <div class="card mt-4">
+            <div class="card-body">
+                <h3 class="card-title">Bosch Sensortec BME680</h3>
+                <div class="card-text text-center">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="iaq" data-label="IAQ" data-size="150" data-width="15" data-theme="Green-Gold-Red"></div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="staticIaq" data-label="Static IAQ" data-size="150" data-width="15" data-theme="Green-Gold-Red" data-total="500" data-showvalue="true"></div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="gas" data-label="Gas" data-append=" Ohm" data-size="150" data-width="15" data-theme="Green-Gold-Red"></div>
+                        </div>
+                    </div>
 
-    <div class="row">
-        <div class="GaugeMeter" id="iaq" data-label="IAQ" data-size="150" data-width="15" data-theme="Green-Gold-Red" data-total="500" data-showvalue="true"></div>
-        <div class="GaugeMeter" id="staticIaq" data-label="Static IAQ" data-size="150" data-width="15" data-theme="Green-Gold-Red" data-total="500" data-showvalue="true"></div>
-        <div class="GaugeMeter" id="gas" data-label="Gas" data-append=" Ohm" data-size="150" data-width="15" data-theme="Green-Gold-Red"></div>
-    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="temp" data-label="Temp" data-size="150" data-width="15" data-theme="Green-Gold-Red"></div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="humid" data-label="Humidity" data-size="150" data-width="15" data-theme="Green-Gold-Red" data-append="%"></div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="GaugeMeter" id="pressure" data-animationstep='0' data-label="Pressure" data-size="150" data-width="15" data-theme="LightBlue-DarkBlue" data-min="650" data-total="1090" data-showvalue="true"></div>
+                        </div>
+                    </div>
 
-    <div class="row">
-        <div class="GaugeMeter" id="temp" data-label="Temp" data-size="150" data-width="15" data-theme="Green-Gold-Red"></div>
-        <div class="GaugeMeter" id="humid" data-label="Humidity" data-size="150" data-width="15" data-theme="Green-Gold-Red" data-append="%"></div>
-        <div class="GaugeMeter" id="pressure" data-label="Pressure" data-size="150" data-width="15" data-theme="LightBlue-DarkBlue" data-min="650" data-total="1090" data-showvalue="true"></div>
-    </div>
+                    <div class="row text-center">
+                        <div class="col-sm-6 gasEquiv">
+                            <div id="co2Equiv" data-label="CO2">
+                                <h5>CO<sup>2</sup></h5>
+                                <span></span> ppm
+                            </div>
+                        </div>
+                        <div class="col-sm-6 gasEquiv">
+                            <div id="vocEquiv" data-label="VOC">
+                                <h5>VOC</h5>
+                                <span></span> ppm
+                            </div>
+                        </div>
+                    </div>
 
-    <div id="co2Equiv" data-label="CO2"><span></span>ppm CO<sup>2</sup></div>
-    <div id="vocEquiv" data-label="VOC"><span></span>ppm VOC</div>
+                    <hr>
 
-    <div class="row" id="infoPanel">
-        <span id="lastUpdated"></span>
-        <span id="uptime"></span>
-        <span id="iaqAccuracy"></span>
-        <span id="statusStab"></span>
-        <span id="statusRunIn"></span>
-    </div>
+                    <div class="row small text-muted text-center mb-2" id="infoPanel">
+                        <div class="col-sm-2">Last Updated: <span id="lastUpdated"></span>
+                        </div>
+                        <div class="col-sm-2">Uptime: <span id="uptime"></span> sec
+                        </div>
+                        <div class="col-sm-2">IAQ Calibrated: <span id="iaqAccuracy"></span>
+                        </div>
+                        <div class="col-sm-2">Status[Stab]: <span id="statusStab"></span>
+                        </div>
+                        <div class="col-sm-2">Status[RunIn]: <span id="statusRunIn"></span>
+                        </div>
+                    </div>
 
-    <div class="row">
-        <div class="col-4">
-            <h3>PHP Client:</h3>
-            <pre><?= json_encode(json_decode($client->sensorBody), JSON_PRETTY_PRINT) ?></pre>
-        </div>
+                    <div class="row">
+                        <button type='button' class="btn btn-secondary" id="showData">Show Raw Data</button>
+                        <div class="col-5 rawdata" id="phpData">
+                            <h3>PHP Client:</h3>
+                            <pre><?= json_encode(json_decode($client->sensorBody), JSON_PRETTY_PRINT) ?></pre>
+                        </div>
 
-        <div class="col-4">
-            <h3>Ajax Client</h3>
-            <pre id="ajaxresult"></pre>
+                        <div class="col-5 rawdata" id="jsData">
+                            <h3>Ajax Client</h3>
+                            <pre id="ajaxresult"></pre>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -63,32 +102,37 @@ $client = new SensorClient("192.168.1.172");
     <script src="/scripts/consciousness.js"></script>
     <script src="/scripts/GaugeMeter.js"></script>
     <script>
-        $(document).ready(function() {
-            $(".GaugeMeter").gaugeMeter();
-
+        function updateSensorData() {
             $.get('http://192.168.1.172', function(data) {
                 $('#ajaxresult').text(JSON.stringify(data, null, 2));
 
                 console.log(data.iaq.toFixed());
 
                 $('#iaq').gaugeMeter({
-                    used: data.iaq.toFixed(),
+                    used: Math.round(data.iaq),
+                    showvalue: true,
+                    min: 0,
+                    total: 500,
+                    animationstep: 0
                 });
 
                 $('#staticIaq').gaugeMeter({
-                    used: data.static_iaq.toFixed()
+                    used: Math.round(data.static_iaq),
+                    animationstep: 0
                 });
 
-                $('#co2Equiv span').text(data.co2_equivalent.toFixed());
+                $('#co2Equiv span').text(parseInt(data.co2_equivalent));
 
                 $('#vocEquiv span').text(data.breath_voc_equivalent.toFixed());
 
                 $('#temp').gaugeMeter({
-                    percent: cToF(data.temperature.comp)
+                    percent: cToF(data.temperature.comp),
+                    animationstep: 0
                 });
 
                 $('#humid').gaugeMeter({
-                    percent: data.humidity.comp
+                    percent: data.humidity.comp,
+                    animationstep: 0
                 });
 
                 $('#pressure').gaugeMeter({
@@ -96,13 +140,37 @@ $client = new SensorClient("192.168.1.172");
                 });
 
                 $('#gas').gaugeMeter({
-                    percent: (data.gas / 5)
+                    percent: data.gas,
+                    animationstep: 0
                 });
-            }, 'json');
 
-            function cToF(celsius) {
-                return cToFahr = celsius * 9 / 5 + 32;
+                $('#lastUpdated').text(data.time.now);
+                $('#uptime').text(data.time.uptime);
+            }, 'json');
+            setTimeout(updateSensorData, 5000);
+        }
+
+        function cToF(celsius) {
+            return cToFahr = celsius * 9 / 5 + 32;
+        }
+
+        function toggleRawData() {
+            console.log('test');
+            if ($('.rawdata').is(':visible')) {
+                console.log('visible');
+                $('.rawdata').hide();
+                return;
             }
+
+            $('.rawdata').show();
+        }
+
+        $(document).ready(function() {
+            $(".GaugeMeter").gaugeMeter();
+            $('.rawdata').hide();
+            updateSensorData();
+
+            $('#showData').on('click', toggleRawData);
         });
     </script>
     <style>
